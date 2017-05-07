b61ae090f6cb376b00a72040f6793476f0f1e246
Suganya <suganya@mindmadetech.com>
2017-05-07 17:47:33 +0530

    commited by suganya after changing circular image

--- Mcom/app/src/main/java/com/mindmade/mcom/activity/SplashActivity.java
+++ Mcom/app/src/main/java/com/mindmade/mcom/activity/SplashActivity.java
@@ -25,7 +25,7 @@ public class SplashActivity extends AppCompatActivity {
             Log.d("suc", "img" + img);
         }
         catch (Exception e){
-            Log.d("suc","ex"+e);
+            Log.d("suc","exe"+e);
         }
         Glide.with(getApplication()).load(R.drawable.splashnewbg)
                 .thumbnail(0.5f)
